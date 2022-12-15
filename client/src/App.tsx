1d8dd34508ff7207283662defb86bda57b02f539
Ziflie <rayanmeziani95@outlook.com>
2022-12-15 12:43:18 +0100

    feature built you can add data to mongodb

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,9 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 type TDeck = {
-  title: string,
-  _id: string, 
-} 
-
+  title: string;
+  _id: string;
+};
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,7 +16,7 @@ function App() {
 
   async function handleCreateDeck(e: React.FormEvent) {
     e.preventDefault();
-    await fetch("http://localhost:5000/decks", {
+    const response = await fetch("http://localhost:5000/decks", {
       method: "POST",
       body: JSON.stringify({
         title,
@@ -26,23 +25,35 @@ function App() {
         "Content-type": "application/json",
       },
     });
+    const deck = await response.json()
+    setDecks([...decks, deck])
     setTitle("");
   }
 
   useEffect(() => {
     async function fetchDecks() {
-     const response = await fetch("http://localhost:5000/decks");
-     const newDecks = await response.json()
-     setDecks(newDecks);
+      const response = await fetch("http://localhost:5000/decks");
+      const newDecks = await response.json();
+      setDecks(newDecks);
     }
     fetchDecks();
   }, []);
 
+  async function handleDeleteDeck(deckId: string) {
+    await fetch(`http://localhost:5000/decks/${deckId}`, {
+      method: "DELETE",
+    });
+    setDecks(decks.filter(deck => deck._id !== deckId))
+  }
+
   return (
     <div className="App">
       <div className="decks">
-        {decks.map((deck) => (
-          <li key={deck._id}>{deck.title}</li>
+        {decks.map(deck => (
+          <div>
+            <li key={deck._id}>{deck.title}</li>
+            <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
+          </div>
         ))}
       </div>
       <form onSubmit={handleCreateDeck}>
